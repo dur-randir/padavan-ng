f65657f5e0bf7860563cf870fd7211e080deb120
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-07-06 09:31:02 +0300

    firmware: add support for ASUS RT-N10C1

---
+++ trunk/configs/boards/ASUS/RT-N10C1/board.h
@@ -0,0 +1,29 @@
+/* ASUS RT-N10 C1 */
+#define BOARD_PID		"RT-N10C1"
+#define BOARD_NAME		"RT-N10C1"
+#define BOARD_DESC		"ASUS RT-N10 C1 Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN10/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	180
+#define BOARD_GPIO_BTN_RESET	10
+#define BOARD_GPIO_BTN_WPS	0
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	9
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_USB
+#define BOARD_GPIO_LED_WPS	13
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_5G_IN_SOC		0
+#define BOARD_2G_IN_SOC		1
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
