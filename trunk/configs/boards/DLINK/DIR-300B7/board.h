9902dc24364c239e7711188c15565196a5d17fa7
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-07-06 09:35:53 +0300

    firmware: add support for DLINK DIR-300B7

---
+++ trunk/configs/boards/DLINK/DIR-300B7/board.h
@@ -0,0 +1,29 @@
+/* Dlink DIR-300 B7 */
+#define BOARD_PID		"DIR-300B7"
+#define BOARD_NAME		"DIR-300B7"
+#define BOARD_DESC		"Dlink DIR-300 B7  Wireless Router"
+#define BOARD_VENDOR_NAME	"Dlink Inc."
+#define BOARD_VENDOR_URL	"http://www.dlink.com/"
+#define BOARD_MODEL_URL		"http://www.dlink.com/Networking/dir300/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	100
+#define BOARD_GPIO_BTN_RESET	10
+#define BOARD_GPIO_BTN_WPS	0
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_POWER
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_WPS
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_5G_IN_SOC		0
+#define BOARD_2G_IN_SOC		1
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
