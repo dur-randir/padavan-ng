cb60bd2613273f4bf35ac58fde4e8054ab9c340a
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-07-06 09:36:17 +0300

    firmware: add support for DLINK DIR-320B1

---
+++ trunk/configs/boards/DLINK/DIR-320B1/board.h
@@ -0,0 +1,30 @@
+/* Dlink DIR-320B1 */
+
+#define BOARD_PID		"DIR-320B1"
+#define BOARD_NAME		"DIR-320B1"
+#define BOARD_DESC		"D-link DIR-320 B1 Wireless Router"
+#define BOARD_VENDOR_NAME	"D-link Inc."
+#define BOARD_VENDOR_URL	"http://www.dlink.com/"
+#define BOARD_MODEL_URL		"http://www.dlink.com/Networking/dir320/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	10
+#define BOARD_GPIO_BTN_WPS	0
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef  BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	9
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_USB	14
+#define BOARD_GPIO_LED_WPS	13
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_5G_IN_SOC		0
+#define BOARD_2G_IN_SOC		1
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
