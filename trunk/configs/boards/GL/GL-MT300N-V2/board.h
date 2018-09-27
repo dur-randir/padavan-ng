939f130217961958d923da2609e835ddb3b6512e
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-09-27 13:45:57 +0000

    add support for GL-MT300N-V2

---
+++ trunk/configs/boards/GL/GL-MT300N-V2/board.h
@@ -0,0 +1,32 @@
+/* GLI MT300N */
+
+#define BOARD_PID		"GL-MT300NV2"
+#define BOARD_NAME		"GL-MT300NV2"
+#define BOARD_DESC		"GL Technologies MT300N Router"
+#define BOARD_VENDOR_NAME	"GL Technologies"
+#define BOARD_VENDOR_URL	"https://www.gl-inet.com"
+#define BOARD_MODEL_URL		"https://www.gl-inet.com/products/gl-mt300n-v2"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	38
+#undef  BOARD_GPIO_BTN_WPS
+#define BOARD_GPIO_BTN_BT1	3	/* switch to the left */
+#define BOARD_GPIO_BTN_BT2	0	/* switch to the right */
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	44	/* hw led */
+#define BOARD_GPIO_LED_POWER	42
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	43
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	2
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
