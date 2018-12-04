4eb4bb5ed84cb86e123c1fa30c2ed04d23095d14
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-12-04 09:00:33 +0000

    firmware: add support for DLINK DIR-620D1

---
+++ trunk/configs/boards/DLINK/DIR-620D1/board.h
@@ -0,0 +1,33 @@
+/* Dlink DIR-620D1 */
+
+#define BOARD_PID		"DIR-620D1"
+#define BOARD_NAME		"DIR-620D1"
+#define BOARD_DESC		"D-link DIR-620 D1 Wireless Router"
+#define BOARD_VENDOR_NAME	"D-link Inc."
+#define BOARD_VENDOR_URL	"http://www.dlink.ru/"
+#define BOARD_MODEL_URL		"http://www.dlink.ru/ru/products/5/1692.html"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_RESET
+#define BOARD_GPIO_BTN_WPS	7
+#undef BOARD_GPIO_BTN_WLTOG
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	17
+#define BOARD_GPIO_LED_POWER	9
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_WPS
+#define BOARD_GPIO_PWR_USB	0
+#define BOARD_GPIO_PWR_USB_ON	0
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_5G_IN_SOC		0
+#define BOARD_2G_IN_SOC		1
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
