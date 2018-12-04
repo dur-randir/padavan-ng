d2a04c3cef0c9766a01048a2b92dfef1c75f845b
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-12-04 08:34:58 +0000

    firmware: add support for ASUS RT-N11P B1

---
+++ trunk/configs/boards/ASUS/RT-N11PB1/board.h
@@ -0,0 +1,28 @@
+/* ASUS RT-N11P B1 */
+
+#define BOARD_PID		"RT-N11PB1"
+#define BOARD_NAME		"RT-N11P_B1"
+#define BOARD_DESC		"ASUS RT-N11P B1 Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RT-N11P-B1/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	90
+#define BOARD_GPIO_BTN_RESET	5
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	44
+#define BOARD_GPIO_LED_POWER	37
+#define BOARD_GPIO_LED_LAN	42
+#define BOARD_GPIO_LED_WAN	43
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
