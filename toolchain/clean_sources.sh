557af2f8a3df2adce643ed49ef20484fe795f4af
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2018-12-24 10:40:03 +0000

    toolchain: clean_sources.sh refactoring

--- toolchain/clean_sources.sh
+++ toolchain/clean_sources.sh
@@ -2,7 +2,23 @@
 
 echo "=================REMOVE-OLD-BUILD-TREE=================="
 
-git clean -xfd ../toolchain
-git checkout ../toolchain
+if [ -f ct-ng ]; then
+	./ct-ng distclean || exit 1
+fi
+
+if [ -f Makefile ]; then
+	make distclean || exit 1
+fi
+
+if [ -d dl ]; then
+        rm -rf dl
+fi
+
+if [ -d out ]; then
+	rm -rf out
+fi
+
+# remove remaining
+rm -rf Makefile.in aclocal.m4 autom4te.cache config.h.in config/gen config/versions configure kconfig/Makefile.in maintainer/package-versions verbatim-data.mk
 
 echo "====================All IS DONE!========================"
