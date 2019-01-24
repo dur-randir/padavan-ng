13a9ff0c414a6c77b781e89c553a2b2a315b0c17
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2019-01-24 16:40:18 +0000

    firmware: add support for TP-LINK TL-WR845N V4

---
+++ trunk/configs/boards/TPLINK/TL_WR845N-V4/board.h
@@ -0,0 +1,35 @@
+/* TP-LINK TL-WR845N V4 */
+
+#define BOARD_PID               "TL_WR845N-V4"
+#define BOARD_NAME              "TL_WR845N-V4"
+#define BOARD_DESC              "TP-Link TL-W845N Wireless Router"
+#define BOARD_VENDOR_NAME       "TP-LINK Technologies CO., LTD."
+#define BOARD_VENDOR_URL        "http://www.tp-link.com/en"
+#define BOARD_MODEL_URL         "http://www.tp-link.com/en/download/TL-WR845N_V4.html"
+#define BOARD_BOOT_TIME         25
+#define BOARD_FLASH_TIME        120
+#define BOARD_GPIO_BTN_RESET    38
+#define BOARD_GPIO_BTN_WPS	37
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	44	/* hw led */
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#define BOARD_GPIO_LED_LAN	40
+#define BOARD_GPIO_LED_WAN      41
+#undef  BOARD_GPIO_LED_WAN_INVERTED
+#define BOARD_GPIO_LED_WAN_PHY	43
+#undef  BOARD_GPIO_LED_WAN_PHY_INVERTED
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC       0
+#define BOARD_NUM_ANT_5G_TX     0
+#define BOARD_NUM_ANT_5G_RX     0
+#define BOARD_NUM_ANT_2G_TX     3
+#define BOARD_NUM_ANT_2G_RX     3
+#define BOARD_NUM_ETH_LEDS      0
+#define BOARD_HAS_EPHY_L1000    0
+#define BOARD_HAS_EPHY_W1000    0
