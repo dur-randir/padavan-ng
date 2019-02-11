d5c7217295ae9f23a514c0729adb1475dbde8de4
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2019-02-11 13:56:50 +0300

    firmware: add support for ZBT-WR8305RT

---
+++ trunk/configs/boards/ZBT/ZBT-WR8305RT/board.h
@@ -0,0 +1,33 @@
+/* Zhibotong ZBT-WR8305RT */
+
+#define BOARD_PID		"ZBT-WR8305RT"
+#define BOARD_NAME		"ZBT-WR8305RT"
+#define BOARD_DESC		"Zhibotong WE8305RT Wireless Router"
+#define BOARD_VENDOR_NAME	"Zhibotong(Shenzhen) Limited"
+#define BOARD_VENDOR_URL	"http://www.zbtlink.com/"
+#define BOARD_MODEL_URL		"http://www.zbtlink.com/products/router/ZBT-WE1626.html"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	1
+#undef BOARD_GPIO_BTN_WPS
+#undef BOARD_GPIO_LED_ALL
+#define	BOARD_GPIO_LED_WIFI	72	/* hw led */
+#undef BOARD_GPIO_LED_SW2G		/* soft led */
+#undef BOARD_GPIO_LED_SW5G		/* soft led */
+#undef BOARD_GPIO_LED_POWER
+#undef BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	38
+#define BOARD_GPIO_LED_USB	39
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC       0
+#define BOARD_NUM_ANT_5G_TX     0
+#define BOARD_NUM_ANT_5G_RX     0
+#define BOARD_NUM_ANT_2G_TX     2
+#define BOARD_NUM_ANT_2G_RX     2
+#define BOARD_NUM_ETH_LEDS      1
+#undef BOARD_ETH_LED_SWAP
+#define BOARD_HAS_EPHY_L1000    0
+#define BOARD_HAS_EPHY_W1000    0
