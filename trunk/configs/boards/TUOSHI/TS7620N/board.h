9804b17329d7000a4459b9c66521ef54d758385c
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2019-03-26 15:34:34 +0300

    firmware: add support for TUOSHI TS7620N

---
+++ trunk/configs/boards/TUOSHI/TS7620N/board.h
@@ -0,0 +1,38 @@
+/* Tuoshi TS7620N */
+
+#define BOARD_PID               "TS7620N"
+#define BOARD_NAME              "TS7620N"
+#define BOARD_DESC              "Tuoshi TS7620N Wireless Router"
+#define BOARD_VENDOR_NAME       "Tuoshi"
+#define BOARD_VENDOR_URL        "http://www.tuoshi.net/"
+#define BOARD_MODEL_URL         "http://www.tuoshi.net/"
+#define BOARD_BOOT_TIME         25
+#define BOARD_FLASH_TIME        120
+#define BOARD_GPIO_BTN_RESET    1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	44	/* hw led */
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_WAN_INVERTED
+#undef  BOARD_GPIO_LED_WAN_PHY
+#undef  BOARD_GPIO_LED_WAN_PHY_INVERTED
+#define BOARD_GPIO_LED_USB	39
+#define BOARD_GPIO_LED_USB_INVERTED
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC       0
+#define BOARD_NUM_ANT_5G_TX     0
+#define BOARD_NUM_ANT_5G_RX     0
+#define BOARD_NUM_ANT_2G_TX     2
+#define BOARD_NUM_ANT_2G_RX     2
+#define BOARD_NUM_ETH_LEDS      1
+#undef BOARD_ETH_LED_SWAP
+#define BOARD_HAS_EPHY_L1000    0
+#define BOARD_HAS_EPHY_W1000    0
+
