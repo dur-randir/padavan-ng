e1e141cbc8a2501d472123b454bccbd98255fddd
Maxim Anisimov <maxim.anisimov.ua@gmail.com>
2019-04-12 09:25:19 +0300

    firmware: add support for SMBXTURBO

---
+++ trunk/configs/boards/SERCOMM/SMBXTURBO/board.h
@@ -0,0 +1,34 @@
+/* BEELINE SMART BOX PRO */
+
+#define BOARD_PID		"SMBXTURBO"
+#define BOARD_NAME		"SMBXTURBO"
+#define BOARD_DESC		"Sercomm board"
+#define BOARD_VENDOR_NAME	"Sercomm"
+#define BOARD_VENDOR_URL	"https://www.sercomm.com/"
+#define BOARD_MODEL_URL		"https://wikidevi.com/wiki/Beeline_SmartBox_Pro/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	14
+#define BOARD_GPIO_BTN_WPS	11
+#undef BOARD_GPIO_BTN_ROUTER
+#define BOARD_GPIO_LED_INVERTED
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_SW2G
+#undef BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER    13
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	8
+#define BOARD_GPIO_LED_USB      15
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_USB_PORT_SWAP	0
