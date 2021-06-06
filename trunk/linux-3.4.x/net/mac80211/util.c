a9a9cce54313744ddbe9471976423fd3d6c9ad8f
Sergey Aleynikov <sergey.aleynikov@gmail.com>
2021-06-06 22:28:16 +0300

    mac80211: allow passing NULL to ieee80211_vif_to_wdev()
    Simply return NULL in this case, instead of crashing. This can
    simplify callers that would otherwise have to check for this
    explicitly.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- trunk/linux-3.4.x/net/mac80211/util.c
+++ trunk/linux-3.4.x/net/mac80211/util.c
@@ -1585,7 +1585,12 @@ void ieee80211_enable_rssi_reports(struct ieee80211_vif *vif,
 				    int rssi_min_thold,
 				    int rssi_max_thold)
 {
-	struct ieee80211_sub_if_data *sdata = vif_to_sdata(vif);
+	struct ieee80211_sub_if_data *sdata;
+
+	if (!vif)
+		return NULL;
+
+	sdata = vif_to_sdata(vif);
 
 	WARN_ON(rssi_min_thold == rssi_max_thold ||
 		rssi_min_thold > rssi_max_thold);
